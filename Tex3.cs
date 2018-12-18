3bd59e312574cd8fa96e64ae86c9bab6bdbc486e
Moonyll <s.pouwel@laposte.net>
2018-12-18 09:02:58 +0100

    Tex3

---
+++ Tex3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         string[] jours = new string[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Jeudi", "Samedi", "Dimanche" };
+         for (int i = 0; i < jours.Length; i++)
+         Console.WriteLine(jours[i]);
+         Console.ReadKey();
+         jours[4] = "Vendredi";
+         for (int i = 0; i < jours.Length; i++)
+         Console.WriteLine(jours[i]);
+         Console.ReadKey();
+        }
+    }
+}
